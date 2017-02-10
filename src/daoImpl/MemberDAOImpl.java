24cf670e6f194f78942c5f4ddba9f846089fbad4
yheisun <yheisun@gmail.com>
2017-02-10 12:30:23 +0900

    daoImpl

--- src/daoImpl/MemberDAOImpl.java
+++ src/daoImpl/MemberDAOImpl.java
@@ -46,9 +46,9 @@ public class MemberDAOImpl implements MemberDAO{
 	@Override
 	public void update(MemberBean member) {
 		try {
-			ResultSet set1=DatabaseFactory.createDatabase(Vendor.ORACLE, Database.USERNAME,Database.PASSWORD).getConnection().createStatement().executeQuery(String.format("SELECT * FROM Member WHERE id = '%s'", member.getId()));
-			while(set1.next()){
-				DatabaseFactory.createDatabase(Vendor.ORACLE, Database.USERNAME,Database.PASSWORD).getConnection().createStatement().executeUpdate(String.format("UPDATE Member SET name='%s',password='%s',profileImg='%s',phone='%s',email='%s' WHERE id='%s'",(member.getName().equals("")?set1.getString("name"):member.getName()),(member.getPassword().equals("")?set1.getString("password"):member.getPassword()),(member.getProfileImg().equals("")?set1.getString("profileImg"):member.getProfileImg()),(member.getPhone().equals("")?set1.getString("phone"):member.getPhone()),(member.getEmail().equals("")?set1.getString("email"):member.getEmail()),member.getId()));
+			ResultSet set=DatabaseFactory.createDatabase(Vendor.ORACLE, Database.USERNAME,Database.PASSWORD).getConnection().createStatement().executeQuery(String.format("SELECT * FROM Member WHERE id = '%s'", member.getId()));
+			while(set.next()){
+				DatabaseFactory.createDatabase(Vendor.ORACLE, Database.USERNAME,Database.PASSWORD).getConnection().createStatement().executeUpdate(String.format("UPDATE Member SET name='%s',password='%s',profileImg='%s',phone='%s',email='%s' WHERE id='%s'",(member.getName().equals("")?set.getString("name"):member.getName()),(member.getPassword().equals("")?set.getString("password"):member.getPassword()),(member.getProfileImg().equals("")?set.getString("profileImg"):member.getProfileImg()),(member.getPhone().equals("")?set.getString("phone"):member.getPhone()),(member.getEmail().equals("")?set.getString("email"):member.getEmail()),member.getId()));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
