278db695d2d4f7d42b39e33dc03559a74840d404
yheisun <yheisun@gmail.com>
2017-02-16 12:45:15 +0900

    17-02-16 12:44 first unit test

--- src/util/JdbcTest.java
+++ src/util/JdbcTest.java
@@ -5,7 +5,8 @@ import java.sql.*;
 import constants.Database;
 
 public class JdbcTest {
-	public static void main(String[] args) {
+	public String getName() {
+		String rs="";
 		try {
 			Class.forName(Database.ORACLE_DRIVER);
 			Connection connection=DriverManager.getConnection(Database.ORACLE_URL,Database.USERNAME,Database.PASSWORD);
@@ -13,12 +14,12 @@ public class JdbcTest {
 			String sql="SELECT * FROM Member WHERE id='hong'",result="";
 			ResultSet set=stmt.executeQuery(sql);
 			while(set.next()){
-				result=set.getString("name");
+				rs=set.getString("name");
 			}
-			System.out.println("회원이름:"+result);
 		} catch (Exception e) {
-			System.out.println("에러발생");
+			rs="에러발생";
 			e.printStackTrace();
 		}
+		return rs;
 	}
 }
